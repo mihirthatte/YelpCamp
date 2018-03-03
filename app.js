0c54acf7302214d53fdd79e6dc0a29ba070ae287
Mihir Thatte <mihir.m.thatte@gmail.com>
2018-03-03 08:48:32 +0000

    A form to add new camp ground and bootstrap cdn, and post request handler has been added

--- app.js
+++ app.js
@@ -5,32 +5,48 @@ app.use(bodyparser.urlencoded({extended:true}));
 app.set('view engine', 'ejs')
 
 
-
-app.get("/", function(req, res){
-   res.render("landing");
-});
-
-app.get("/campgrounds", function(req, res){
-	var campground = [
+var campground = [
 		{ name: "Clifty Falls State Park", image:"http://www.photosforclass.com/download/pixabay-839807"},
 		{ name: "Pokagon State Park", image:"http://www.photosforclass.com/download/pixabay-2788677"},
 		{ name: "Hardin Ridge Recreation Area", image:"http://www.photosforclass.com/download/pixabay-3111921"},
 		{ name: "McCormick's Creek State Park", image:"http://www.photosforclass.com/download/pixabay-2692058"},
 		{ name: "Turkey Run State Park", image:"http://www.photosforclass.com/download/pixabay-2513008"}
-	];
+];
+	
+app.get("/", function(req, res){
+   res.render("landing");
+});
+
+app.get("/campgrounds", function(req, res){
 	res.render("campgrounds", {campground:campground});	
 
+});
+
+app.get("/campgrounds/new", function(req, res){
+	res.render("new_form");	
+});
+
+
+app.post("/campgrounds", function(req, res){
+	//get data from post request and add to campground array
+	var name = req.body.name;
+	var image = req.body.image;
+	campground.push({name:name, image:image});
+	res.redirect("/campgrounds")
 });
 
 
 
 
+//When running on aws - 
+//app.listen(3000, function () {
+//  console.log('Yelp Camp app listening on port 3000!')
+//});
 
-
-
-app.listen(3000, function () {
-  console.log('Yelp Camp app listening on port 3000!')
-})
+//When running on cloud 9 - 
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log('Yelp Camp app listening on port'+process.env.PORT);
+});
 
 
 
